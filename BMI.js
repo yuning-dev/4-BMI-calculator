6563a05af659f8cfd3efe8ada075df3dc2b435fd
yuning-dev <yuning.eng@gmail.com>
2023-07-26 13:46:52 +0100

    formatted js so different results show in different colours

--- BMI.js
+++ BMI.js
@@ -26,27 +26,23 @@ function calculateBMI(scalingFactor) {
 
     var BMI = scalingFactor * userWeight / (userHeight * userHeight)
     if (BMI < 18.5) {
-        var div = document.createElement("div")
-        var divText = document.createTextNode("You are underweight. Please consider one of our tailored diet plans.")
-        div.appendChild(divText)
-        document.getElementById("submit").appendChild(div)
+        var resultDiv = document.getElementById("result")
+        resultDiv.innerText = "You are underweight. Please consider one of our tailored diet plans."
+        resultDiv.className = "underweightResult"
     }
     else if (BMI < 25) {
-        var div = document.createElement("div")
-        var divText = document.createTextNode("You have a healthy weight. Keep up!")
-        div.appendChild(divText)
-        document.getElementById("submit").appendChild(div)
+        var resultDiv = document.getElementById("result")
+        resultDiv.innerText = "You have a healthy weight. Keep up!"
+        resultDiv.className = "normalWeightResult"
     }
     else if (BMI < 30) {
-        var div = document.createElement("div")
-        var divText = document.createTextNode("You are overweight. Please consider one of our tailored diet plans.")
-        div.appendChild(divText)
-        document.getElementById("submit").appendChild(div)
+        var resultDiv = document.getElementById("result")
+        resultDiv.innerText = "You are overweight. Please consider one of our tailored diet plans."
+        resultDiv.className = "overweightResult"  
     }
     else {
-        var div = document.createElement("div")
-        var divText = document.createTextNode("You are obese. Please check out one of our tailored diet plans. If you would like to contact one of our specialists, here's a 15% voucher for the first 3 sessions.")
-        div.appendChild(divText)
-        document.getElementById("submit").appendChild(div)
+        var resultDiv = document.getElementById("result")
+        resultDiv.innerText = "You are obese. Please check out one of our tailored diet plans. If you would like to contact one of our specialists, here's a 15% voucher for the first 3 sessions."
+        resultDiv.className = "obeseResult"
     }
 }
