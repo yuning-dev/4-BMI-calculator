f047890aacf9ce4e0b9a86330b58d4c8133b43ff
yuning-dev <yuning.eng@gmail.com>
2023-08-14 11:59:21 +0100

    added functionality so if user enters values and then changes the unit system, the values entered will be converted to the new unit system

--- BMI.js
+++ BMI.js
@@ -34,7 +34,7 @@ function calculateBMI(scalingFactor) {
     else if (BMI < 30) {
         setResultText(String.fromCodePoint(0x1F6D1) + " " + "You are overweight. Please consider one of our tailored diet plans.", "overweightResult"  )
     }
-    else {
+    else if (BMI >= 30) {
         setResultText(String.fromCodePoint(0x1F974) + " " + "You are obese. Please check out one of our tailored diet plans. If you would like to contact one of our specialists, here's a 15% voucher for the first 3 sessions.", "obeseResult")
     }
 }
@@ -43,4 +43,41 @@ function setResultText(resultText, className) {
     var resultDiv = document.getElementById("result")
     resultDiv.innerText = resultText
     resultDiv.className = className
+}
+
+function metricToImperial() {
+    let metricWeight = Number(document.getElementById("weight").value)
+    let convertedWeight = metricWeight * 2.20462
+    document.getElementById("weight").value = convertedWeight
+    let metricHeight = Number(document.getElementById("height").value)
+    let convertedHeight = metricHeight * 39.3701
+    document.getElementById("height").value = convertedHeight
+}
+
+function imperialToMetric() {
+    let imperialWeight = Number(document.getElementById("weight").value)
+    let convertedWeight = imperialWeight / 2.20462
+    document.getElementById("weight").value = convertedWeight
+    let imperialHeight = Number(document.getElementById("height").value)
+    let convertedHeight = imperialHeight / 39.3701
+    document.getElementById("height").value = convertedHeight
+}
+
+function roundAndTidy() {
+    let weight = Number(document.getElementById("weight").value)
+    let height = Number(document.getElementById("height").value)
+    document.getElementById("weight").value = weight.toFixed(2)
+    document.getElementById("height").value = height.toFixed(2)
+}
+
+function imperial() {
+    addImperialText()
+    metricToImperial()
+    roundTo2Decimals()
+}
+
+function metric() {
+    addMetricText()
+    imperialToMetric()
+    roundTo2Decimals()
 }
